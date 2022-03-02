ea1073cf4ff48579c6a09d324727518ba6f6b48a
Chase Stokes <chasehunterstokes@gmail.com>
2022-03-02 11:29:38 -0800

    pass list test
    Co-authored-by: jakechaley <jakechaley@users.noreply.github.com>

--- PYB.Tests/ModelTests/PlacesTests.cs
+++ PYB.Tests/ModelTests/PlacesTests.cs
@@ -12,9 +12,11 @@ namespace PYB.Tests
     [TestMethod]
     public void GetAll_ReturnsEmptyList_ItemList()
     {
-      string description = "It was cool";
-      string cityName ="portland";
-      Places newPlace = new Places(cityName, description);
+      // --- Needed for good fail on test---
+      // string description = "It was cool";
+      // string cityName ="portland";
+      // Places newPlace = new Places(cityName, description);
+      // ------------------------------------------------------------
       List<Places> newPlaceList = new List<Places> {};
       List<Places> result = Places.GetAll();
       CollectionAssert.AreEqual(newPlaceList, result);
