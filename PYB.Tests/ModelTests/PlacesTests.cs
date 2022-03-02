28b1af8e0a6ba200a70da9d8a6808695ae56e398
Chase Stokes <chasehunterstokes@gmail.com>
2022-03-02 11:34:03 -0800

    good fail on ID test
    Co-authored-by: jakechaley <jakechaley@users.noreply.github.com>

--- PYB.Tests/ModelTests/PlacesTests.cs
+++ PYB.Tests/ModelTests/PlacesTests.cs
@@ -21,5 +21,18 @@ namespace PYB.Tests
       List<Places> result = Places.GetAll();
       CollectionAssert.AreEqual(newPlaceList, result);
     }
+
+    [TestMethod]
+    public void Find_ReturnCorrectItem_Place()
+    {
+      string description = "It was cool";
+      string cityName ="portland";
+      string description1 = "It was wet";
+      string cityName1 ="seattle";
+      Places newPlace = new Places(cityName, description);
+      Places newPlace1 = new Places(cityName1, description1);
+      Places result = Places.Find(2); // change the number in Find() for good fail
+      Assert.AreEqual(newPlace, result);
+    }
   }
 }
